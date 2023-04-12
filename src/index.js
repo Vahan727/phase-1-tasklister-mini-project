b4e73f67551b2ab8ca4308161e53bb093cdea633
Vahan Nadjarian <vahan@nadjarian.com>
2023-04-12 18:06:14 -0400

    completed Task Lister

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+}
+);
+function renderTask(text) {
+  const li = document.createElement('li')
+  li.className = 'New Task'
+  li.textContent = text
+  document.querySelector('#tasks').appendChild(li)
+}
+const form = document.querySelector("form")
+form.addEventListener("submit", (e) => handleTask(e))
+function handleTask(event) {
+  event.preventDefault()
+  const input = document.querySelector('input').value
+  renderTask(input)
+}
